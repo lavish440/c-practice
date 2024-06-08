b747e0a4067e075e13e78b0aa8a3f58c6530c428
Lavish <64689132+lavish440@users.noreply.github.com>
2024-06-08 19:05:03 +0530

    Update commit_solution.sh script to improve functionality
    - Ensure the question description is wrapped to 50
      characters per line.
    - Add opening the specific exercise file in nvim for
      editing before staging.
    - Concatenate lines in the description with proper
      spacing.
    - Enhance the success message with colored and
      distinct exercise and question numbers.

--- commit_solution.sh
+++ commit_solution.sh
@@ -8,6 +8,8 @@ fi
 EXERCISE=$1
 QUESTION=$2
 
+nvim exercise_"$EXERCISE"/question_"$QUESTION".c
+
 git add exercise_"$EXERCISE"/question_"$QUESTION".c
 
 echo "Enter the question description: "
@@ -18,11 +20,11 @@ while true; do
 	if [[ -z "$LINE" ]]; then
 		break
 	fi
-	DESC="$DESC$LINE"
+	DESC="$DESC $LINE"
 done
 
 DESC="$(echo "$DESC" | fold -s -w 50)"
 
 git commit -m "[Exercise $EXERCISE] Solved Exercise $EXERCISE, Question $QUESTION" -m "Question: $DESC"
 
-echo "Successfully committed Exercise $EXERCISE, Question $QUESTION."
+echo -e "\033[38;2;0;255;0mSuccessfully committed Exercise \033[38;2;0;255;255m$EXERCISE\033[38;2;0;255;0m, Question \033[38;2;0;255;255m$QUESTION\033[0;00m.\033[0;00m"
