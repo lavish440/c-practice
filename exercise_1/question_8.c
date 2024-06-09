eb3030041fb8a3b369db76f51e2a344841046e0f
Lavish <64689132+lavish440@users.noreply.github.com>
2024-06-09 23:04:50 +0530

    [Exercise 1] Solved Exercise 1, Question 8
    Question:  Write a program to count blanks, tabs, and
    newlines.

---
+++ exercise_1/question_8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int blanks = 0;
+  int tabs = 0;
+  int nl = 0;
+
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      blanks++;
+    } else if (c == '\t') {
+      tabs++;
+    } else if (c == '\n') {
+      nl++;
+    }
+  }
+
+  printf("Blank Lines: %d\n", blanks);
+  printf("Tabs: %d\n", tabs);
+  printf("New Lines: %d\n", nl);
+  return EXIT_SUCCESS;
+}
