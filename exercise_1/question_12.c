d9056a2d907ef34ff2c10ae690f86656706b0bbc
Lavish <64689132+lavish440@users.noreply.github.com>
2024-06-12 23:15:19 +0530

    [Exercise 1] Solved Exercise 1, Question 12
    Question:  Write a program that prints its input one word
    per line.

---
+++ exercise_1/question_12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      putchar('\n');
+    } else {
+      putchar(c);
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
