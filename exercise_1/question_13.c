f091a2ac706e6885802b1d12ca1bf25fd00b903d
Lavish <64689132+lavish440@users.noreply.github.com>
2024-06-21 19:41:33 +0530

    Formatting

--- exercise_1/question_13.c
+++ exercise_1/question_13.c
@@ -5,37 +5,43 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-int main(void) {
-  int c, count = 0;
-  int word_length[MAX_WORD_LENGTH + 1];
+int main(void)
+{
+    int c, count = 0;
+    int word_length[MAX_WORD_LENGTH + 1];
 
-  for (int i = 0; i <= MAX_WORD_LENGTH; word_length[i++] = 0)
-    ;
+    for (int i = 0; i <= MAX_WORD_LENGTH; word_length[i++] = 0)
+        ;
 
-  while ((c = getchar()) != EOF) {
-    if (c == ' ' || c == '\n' || c == '\t') {
-      word_length[MIN(count, MAX_WORD_LENGTH)]++;
-      count = 0;
-    } else {
-      count++;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            word_length[MIN(count, MAX_WORD_LENGTH)]++;
+            count = 0;
+        }
+        else
+        {
+            count++;
+        }
     }
-  }
 
-  for (int i = MAX_FREQUENCY; i >= 1; i--) {
-    printf("\n");
-    printf("%02d | ", i);
-    for (int j = 1; j <= MAX_WORD_LENGTH; j++)
-      printf(word_length[j] >= i ? "*  " : "   ");
-  }
+    for (int i = MAX_FREQUENCY; i >= 1; i--)
+    {
+        printf("\n");
+        printf("%02d | ", i);
+        for (int j = 1; j <= MAX_WORD_LENGTH; j++)
+            printf(word_length[j] >= i ? "*  " : "   ");
+    }
 
-  printf("\n   +");
-  for (int i = 1; i <= MAX_WORD_LENGTH; i++)
-    printf("———");
+    printf("\n   +");
+    for (int i = 1; i <= MAX_WORD_LENGTH; i++)
+        printf("———");
 
-  printf("\nF/L ");
+    printf("\nF/L ");
 
-  for (int i = 1; i <= MAX_WORD_LENGTH; i++)
-    printf("%02d ", i);
+    for (int i = 1; i <= MAX_WORD_LENGTH; i++)
+        printf("%02d ", i);
 
-  return 0;
+    return 0;
 }
--- exercise_1/question_14.c
+++ exercise_1/question_14.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 
-int main() {
-  char histogram[95];
-  int c, i, j;
+int main()
+{
+    char histogram[95];
+    int  c, i, j;
 
-  for (i = 0; i < 95; ++i)
-    histogram[i] = 0;
+    for (i = 0; i < 95; ++i)
+        histogram[i] = 0;
 
-  while ((c = getchar()) != EOF)
-    if (c >= 32 || c <= 95)
-      ++histogram[c - 32];
+    while ((c = getchar()) != EOF)
+        if (c >= 32 || c <= 95)
+            ++histogram[c - 32];
 
-  int maximum = histogram[0];
+    int maximum = histogram[0];
 
-  for (i = 0; i < 95; ++i)
-    if (histogram[i] > maximum)
-      maximum = histogram[i];
+    for (i = 0; i < 95; ++i)
+        if (histogram[i] > maximum)
+            maximum = histogram[i];
 
-  for (i = maximum; i > 0; --i) {
-    printf("%4d|", i);
-    for (j = 0; j < 95; ++j) {
-      if (histogram[j] == i) {
-        putchar('*');
-        --histogram[j];
-      } else
-        putchar(' ');
+    for (i = maximum; i > 0; --i)
+    {
+        printf("%4d|", i);
+        for (j = 0; j < 95; ++j)
+        {
+            if (histogram[j] == i)
+            {
+                putchar('*');
+                --histogram[j];
+            }
+            else
+                putchar(' ');
+        }
+        putchar('\n');
     }
+
+    printf("    +");
+    for (i = 0; i < 95; i++)
+        putchar('-');
+
+    printf("\n     ");
+    for (i = 32; i <= 126; ++i)
+        putchar(i);
+
     putchar('\n');
-  }
 
-  printf("    +");
-  for (i = 0; i < 95; i++)
-    putchar('-');
-
-  printf("\n     ");
-  for (i = 32; i <= 126; ++i)
-    putchar(i);
-
-  putchar('\n');
-
-  return 0;
+    return 0;
 }
