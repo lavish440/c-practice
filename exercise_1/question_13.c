6f9e37e4370472fd5de7db4e578b316ac9e1a024
Lavish <64689132+lavish440@users.noreply.github.com>
2024-06-21 19:06:35 +0530

    [Exercise 1] Solved Exercise 1, Question 13
    Question:  Write a program to print a histogram of the
    lengths of words in its input. It is easy to draw
    the histogram with the bars horizontal; a
    vertical orientation is more challenging

---
+++ exercise_1/question_13.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define MAX_WORD_LENGTH 8
+#define MAX_FREQUENCY 6
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+int main(void) {
+  int c, count;
+  int word_length[MAX_WORD_LENGTH + 1];
+
+  for (int i = 0; i <= MAX_WORD_LENGTH; word_length[i++] = 0)
+    ;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+      word_length[MIN(count, MAX_WORD_LENGTH)]++;
+      count = 0;
+    } else {
+      count++;
+    }
+  }
+
+  for (int i = MAX_FREQUENCY; i >= 1; i--) {
+    printf("\n");
+    printf("%02d | ", i);
+    for (int j = 1; j <= MAX_WORD_LENGTH; j++)
+      printf(word_length[j] >= i ? "*  " : "   ");
+  }
+
+  printf("\n   +");
+  for (int i = 1; i <= MAX_WORD_LENGTH; i++)
+    printf("———");
+
+  printf("\nF/L ");
+
+  for (int i = 1; i <= MAX_WORD_LENGTH; i++)
+    printf("%02d ", i);
+
+  return 0;
+}
